9c6a74ddbbb84c375e1234b6dfd1e442e1d6ad04
Herzanich Georgii <109143250+Fenix524@users.noreply.github.com>
2024-01-21 12:19:51 +0100

    fix first task

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,23 @@ import TaskTitle from './components/TaskTitle/TaskTitle';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
-import userData from './assets/userData.json'
-import friendsArray from './assets/friends.json'
-import transactionArray from './assets/transactions.json'
-
+import userData from './assets/userData.json';
+import friendsArray from './assets/friends.json';
+import transactionArray from './assets/transactions.json';
 
 function App() {
   return (
     <>
-     <TaskTitle text="Завдання 1 - Профіль соціальної мережі"/>
-     <Profile profileData={userData} />
+      <TaskTitle text="Завдання 1 - Профіль соціальної мережі" />
+      <Profile name={userData.username} tag={userData.tag} location={userData.location} image={userData.avatar} stats={userData.stats} />
 
-     <TaskTitle text="Завдання 2 - Список друзів"/>
-     <FriendList friends={friendsArray}/>
+      <TaskTitle text="Завдання 2 - Список друзів" />
+      <FriendList friends={friendsArray} />
 
-     <TaskTitle text="Завдання 3 - Історія транзакцій"/>
-     <TransactionHistory items={transactionArray} />
+      <TaskTitle text="Завдання 3 - Історія транзакцій" />
+      <TransactionHistory items={transactionArray} />
     </>
   );
 }
-
-
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,32 @@
-import Description from '../Description/Description';
-import StatsList from '../StatsList/StatsList';
 import css from './Profile.module.css';
 
-const Profile = ({ profileData }) => (
+const Profile = ({ name, tag, location, image, stats }) => (
   <div className={css.profile}>
-    <Description obj={profileData} />
-    <StatsList stats={profileData.stats} />
+    <div className={css.description}>
+      <img
+        src={image}
+        alt="User avatar"
+        className={css.avatar}
+      />
+      <p className={css.name}>{name}</p>
+      <p className={css.tag}>@{tag}</p>
+      <p className={css.location}>{location}</p>
+    </div>
+
+    <ul className={css.stats}>
+      <li>
+        <span className={css.label}>Followers</span>
+        <span className={css.value}>{stats.followers}</span>
+      </li>
+      <li>
+        <span className={css.label}>Views</span>
+        <span className={css.value}>{stats.views}</span>
+      </li>
+      <li>
+        <span className={css.label}>Likes</span>
+        <span className={css.value}>{stats.likes}</span>
+      </li>
+    </ul>
   </div>
 );
 
